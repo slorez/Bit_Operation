92751ff90504bcea1db229bfd4b8006d6f48e629
Airat Mullayanov <slorez189@gmail.com>
2022-12-08 09:12:55 +0500

    Bitwise operation: Write log 1 to digits 7 6. and through inverting, reset the 7 6 digit

--- src/main.c
+++ src/main.c
@@ -31,9 +31,13 @@ int main(int argc, char const *argv[])
 	putchar('\n');
 	
 	/* Запись лог 0 с записью остальных разрядов в лог 1 */
+	// 1100.0000
+	uint8_t dev = (1<<7) | (1<<6);
+	// 1101.1101
 	b = 0xDD;
 	printBit(b);
-	b = ~(1<<7);
+	// 0011.1111 = 0x3F;
+	b = ~(dev);
 	printf(" b: %#x ", b);
 	printBit(b);
 	putchar('\n');
